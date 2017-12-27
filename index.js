3158887c468c6a3b0773d21b643eeb85b1523ebb
kdm <kennygprs@gmail.com>
2017-12-27 13:18:00 -0500

    Attempting to add AVRCP profile

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-
 const Bluez = require('./lib/Bluez');
 
+Bluez.Bluez = Bluez;
 
 Bluez.Adapter = require('./lib/Adapter');
-Bluez.Agent = require('./lib/Agent');
-Bluez.Bluez = Bluez;
-Bluez.Device = require('./lib/Device');
+Bluez.Agent   = require('./lib/Agent');
+Bluez.Device  = require('./lib/Device');
 Bluez.Profile = require('./lib/Profile');
-Bluez.SerialProfile = require('./lib/SerialProfile');
+
 Bluez.RawFdSocket = require('./lib/RawFdSocket');
+
+Bluez.AVRCProfile   = require('./lib/AVRCProfile');
+Bluez.SerialProfile = require('./lib/SerialProfile');
 
 module.exports = Bluez;
