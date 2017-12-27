5d16deb48a7d6c533ea93e7fb8ded74f89e1f039
kdm <kennygprs@gmail.com>
2017-12-27 13:28:14 -0500

    Update

--- examples/serial.js
+++ examples/serial.js
@@ -1,21 +1,23 @@
+/* eslint no-console : 0 */
+
 const Bluez = require('../index');
 
 const bluetooth = new Bluez();
 
 // Register callback for new devices
 bluetooth.on('device', async (address, props) => {
-	console.log("Found new Device " + address + " " + props.Name);
+	console.log('Found new Device ' + address + ' ' + props.Name);
 	// apply some filtering
-	if(props.Name !== "kdm-cell") return;
+	if (props.Name !== 'kdm-cell') return;
 
 	// Get the device interface
 	const device = await bluetooth.getDevice(address);
 
 	// Pair with the device if not already done
 	// Not pairing twice will throw an error
-	if(!props.Paired) {
-		await device.Pair().catch((err)=>{
-			console.error("Error while pairing to device " + address + ": " + err.message);
+	if (!props.Paired) {
+		await device.Pair().catch((err) => {
+			console.error('Error while pairing to device ' + address + ': ' + err.message);
 		});
 	}
 
@@ -26,33 +28,32 @@ bluetooth.on('device', async (address, props) => {
 	}
 
 	// Connect to the Serial Service
-	await device.ConnectProfile(Bluez.SerialProfile.uuid).catch((err)=>{
-		console.error("Error while connecting serial profile to device " + address + ": " + err.message);
+	await device.ConnectProfile(Bluez.SerialProfile.uuid).catch((err) => {
+		console.error('Error while connecting serial profile to device ' + address + ': ' + err.message);
 	});
 });
 
-bluetooth.init().then(async ()=>{
-
+bluetooth.init().then(async () => {
 	// Register Agent that accepts everything and uses key 1234
 	await bluetooth.registerDefaultAgent();
-	console.log("Agent registered");
+	console.log('Agent registered');
 
 	// Register a Serial Client Service
 	await bluetooth.registerSerialProfile(async (device, socket) => {
 		const name = await device.Name();
-		console.log("Serial Connection from " + name);
+		console.log('Serial Connection from ' + name);
 		// socket is a non blocking duplex stream similar to net.Socket
 		// Print everything
 		socket.pipe(process.stdout);
-		//socket.on('data', (data)=>process.stdout.write(data));
+		// socket.on('data', (data)=>process.stdout.write(data));
 		socket.on('error', console.error);
 		// Send hello to device
-		socket.write("Hello\n");
-	}, "client");
-	console.log("SerialProfile registered");
+		socket.write('Hello\n');
+	}, 'client');
+	console.log('SerialProfile registered');
 
 	// listen on first bluetooth adapter
 	const adapter = await bluetooth.getAdapter('hci0');
 	await adapter.StartDiscovery();
-	console.log("Discovering");
+	console.log('Discovering');
 });
